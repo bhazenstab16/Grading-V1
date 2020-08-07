726b500b5a59ecbe37b047f62633c96bbf625fae
brandonIT <59006787+brandonIT@users.noreply.github.com>
2020-08-07 15:17:14 -0400

    Update Program.cs

--- Grading V1/Program.cs
+++ Grading V1/Program.cs
@@ -8,173 +8,77 @@ namespace Grading_Again_Lol
 {
     class Program
     {
-        public static void printDirections()
+        public static void printDirections() 
         {
-            Console.WriteLine("Welcome to the grading machine.");
-            Console.WriteLine("");
-            Console.WriteLine("This program is designed to convert your grades (in numbers) to their letter version counter parts.");
-            Console.WriteLine("");
-            Console.WriteLine("The program will continue to read in grades until you enter -1.");
-            Console.WriteLine("");
-            Console.WriteLine("Enjoy!");
-            Console.WriteLine("");
+            Console.WriteLine("Welcome to the grading machine.\nThis program is designed to convert your grades (in numbers) to their letter version counter parts.\nThe program will continue to read in grades until you enter -1.\nEnjoy!\n");
         }
         public static int getInt()
         {
             int num = 0;
             string input = "";
-
-            Console.WriteLine("");
-            Console.WriteLine("Please enter an integer: ");
-            Console.WriteLine("");
-
+            Console.WriteLine("\nPlease enter an integer: \n");
             input = Console.ReadLine();
             num = Convert.ToInt32(input);
             return num;
-
         }
         public static void grading()
         {
             int grade = 0;
             int gradeCount = 0;
             int gradeSum = 0;
-            //int highestGrade = 0;
-           // int lowestGrade = 0;
             double gradeAverage = 0.0;
             char gradeValue = 'A';
-
             printDirections();
-
             grade = getInt();
-
             switch (grade)
             {
                 case -1:
                     break;  
             }
-
-            //lowestGrade = grade;
-
             while (grade != -1)
             {
                 if ((grade <= 100) && (grade >= 90))
                 {
                     gradeCount++;
                     gradeSum += grade;
-
-                    Console.WriteLine("");
-                    Console.WriteLine("The grade you entered is an A!");
-                    Console.WriteLine("");
-
-                    /*if(grade < lowestGrade)
-                    {
-                        lowestGrade = grade;
-                    }
-
-                    if(grade > highestGrade)
-                    {
-                        highestGrade = grade;
-                    }
-                    */
+                    Console.WriteLine("\nThe grade you entered is an A!\n");
                     grade = getInt();
                 }
                 else if ((grade < 90) && (grade >= 80))
                 {
                     gradeCount++;
                     gradeSum += grade;
-
-                    Console.WriteLine("");
-                    Console.WriteLine("The grade you entered is a B!");
-                    Console.WriteLine("");
-                    
-                    /*if(grade < lowestGrade)
-                    {
-                        lowestGrade = grade;
-                    }
-
-                    if(grade > highestGrade)
-                    {
-                        highestGrade = grade;
-                    }
-                    */
+                    Console.WriteLine("\nThe grade you entered is a B!\n");
                     grade = getInt();
-
                 }
                 else if ((grade < 80) && (grade >= 70))
                 {
                     gradeCount++;
                     gradeSum += grade;
-
-                    Console.WriteLine("");
-                    Console.WriteLine("The grade you entered is a C.");
-                    Console.WriteLine("");
-                    
-                    /*if(grade < lowestGrade)
-                    {
-                        lowestGrade = grade;
-                    }
-
-                    if(grade > highestGrade)
-                    {
-                        highestGrade = grade;
-                    }
-                    */
+                    Console.WriteLine("\nThe grade you entered is a C.\n");
                     grade = getInt();
                 }
                 else if ((grade < 70) && (grade >= 60))
                 {
                     gradeCount++;
                     gradeSum += grade;
-
-                    Console.WriteLine("");
-                    Console.WriteLine("The grade you entered is a D.");
-                    Console.WriteLine("");
-                    
-                    /*if(grade < lowestGrade)
-                    {
-                        lowestGrade = grade;
-                    }
-
-                    if(grade > highestGrade)
-                    {
-                        highestGrade = grade;
-                    }
-                    */
+                    Console.WriteLine("\nThe grade you entered is a D.\n");
                     grade = getInt();
                 }
                 else if ((grade < 70) && (grade >= 1))
                 {
                     gradeCount++;
                     gradeSum += grade;
-
-                    Console.WriteLine("");
-                    Console.WriteLine("The grade you entered is an F.");
-                    Console.WriteLine("");
-                    
-                    /*if(grade < lowestGrade)
-                    {
-                        lowestGrade = grade;
-                    }
-
-                    if(grade > highestGrade)
-                    {
-                        highestGrade = grade;
-                    }
-                    */
+                    Console.WriteLine("\nThe grade you entered is an F.\n");
                     grade = getInt();
                 }
                 else
                 {
-                    Console.WriteLine("");
-                    Console.WriteLine("You have entered an invalid grade, please try again");
-                    Console.WriteLine("");
-
+                    Console.WriteLine("\nYou have entered an invalid grade, please try again\n");
                     grade = getInt();
                 }
             }
-
             gradeAverage = (double)gradeSum / gradeCount;
-
             if ((gradeAverage <= 100) && (gradeAverage >= 90))
             {
                 gradeValue = 'A';
@@ -195,28 +99,15 @@ namespace Grading_Again_Lol
             {
                 gradeValue = 'F';
             }
-            else
-            {
-                Console.WriteLine("");
-                Console.WriteLine("Error in average ifs.");
-                Console.WriteLine("");
-            }
-            
-            Console.WriteLine("");
-            Console.WriteLine("The average of the grades you entered (in numerical form) is " + gradeAverage + ".");
-            Console.WriteLine("");
-            
+            Console.WriteLine("\nThe average of the grades you entered (in numerical form) is " + gradeAverage + ".\n");
             if ((gradeValue == 'A') || (gradeValue == 'F'))
             {
-                Console.WriteLine("The letter grade equivalent to " + gradeAverage + " is an " + gradeValue + ".");
-                Console.WriteLine("");
+                Console.WriteLine("\nThe letter grade equivalent to " + gradeAverage + " is an " + gradeValue + ".\n");
             }
             else
             {
-                Console.WriteLine("The letter grade equivalent to " + gradeAverage + " is a " + gradeValue + ".");
-                Console.WriteLine("");
+                Console.WriteLine("\nThe letter grade equivalent to " + gradeAverage + " is a " + gradeValue + ".\n");
             }
-            
         }
         static void Main(string[] args)
         {
